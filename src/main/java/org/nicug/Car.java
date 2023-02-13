839e4e847abf4ab6643d044aac1c984dc2a3f839
Nicug-bit <niculaegrb@gmail.com>
2023-02-13 10:24:36 +0200

    new cars

---
+++ src/main/java/org/nicug/Car.java
@@ -0,0 +1,15 @@
+package org.nicug;
+
+public class Car {
+    // instance variables
+    String name;
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+
+}
